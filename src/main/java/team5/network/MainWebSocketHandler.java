d00488f212b678286d7631a0e6401469314cc52e
Jman012 <jman012guy@gmail.com>
2017-03-08 15:24:23 -0800

    Implement the simple routing in the web socket handler

--- src/main/java/team5/network/MainWebSocketHandler.java
+++ src/main/java/team5/network/MainWebSocketHandler.java
@@ -1,6 +1,7 @@
 package team5.network;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import team5.game.GameManagerSingleton;
 
 
 /**
@@ -14,17 +15,28 @@ public class MainWebSocketHandler {
     @OnWebSocketConnect
     public void onConnect(Session session) throws Exception {
         System.out.println("New connection: " + session.getRemoteAddress());
+
+        // Have the GameManagerSingleton create a new CommBridge for us,
+        // passing our Session to it
+        //GameManagerSingleton.sharedInstance.newCommBridge(session);
     }
 
     @OnWebSocketClose
     public void onClose(Session session, int statusCode, String reason) {
         System.out.println("Closed connection: " + statusCode + " " + session.getRemoteAddress() + " " + reason);
 
+        // Have the singleton lose the CommBridge
+        //GameManagerSingleton.sharedInstance.destroyCommBridge(session)
     }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
         System.out.println("New message: " + session.getRemoteAddress() + " " + message);
-    }
 
+        // Get the CommBridge
+        //CommunicationBridge commBridge = GameManagerSingleton.sharedInstance.commBridge(session);
+
+        // Pass on the JSON message (still as String)
+        //commBridge.parseMessage(message);
+    }
 }
