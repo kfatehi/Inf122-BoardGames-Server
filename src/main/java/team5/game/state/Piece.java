bed4e8d1310640664a141fbb897d0a95be4fae38
hero <alvinherot@gmail.com>
2017-03-09 20:09:08 -0800

    Added NEXT_UNIQUE_ID variable to piece

--- src/main/java/team5/game/state/Piece.java
+++ src/main/java/team5/game/state/Piece.java
@@ -4,6 +4,12 @@ package team5.game.state;
  * Created by james on 3/8/17.
  */
 public class Piece {
+    private static int NEXT_UNIQUE_ID = 0;
+    
+    private int id;
 
-    public Piece() {}
+    public Piece() {
+        this.id = Piece.NEXT_UNIQUE_ID; 
+        Piece.NEXT_UNIQUE_ID += 1;
+    }
 }
