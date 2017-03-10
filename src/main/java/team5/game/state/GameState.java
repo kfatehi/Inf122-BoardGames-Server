b51f90db640ad3dd99f7ce20d18645726a8ef4b7
Jman012 <jman012guy@gmail.com>
2017-03-10 12:09:04 -0800

    Skeleton of GameState

--- src/main/java/team5/game/state/GameState.java
+++ src/main/java/team5/game/state/GameState.java
@@ -1,9 +1,32 @@
 package team5.game.state;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by james on 3/8/17.
  */
 public class GameState {
 
-    public GameState() {}
+    private Board board;
+    private PiecePool serverPool = new PiecePool();
+    private PiecePool capturedPool = new PiecePool();
+    private Map<String, PiecePool> userPools = new HashMap<String, PiecePool>();
+
+    public GameState(int rows, int cols) {
+        board = new Board(rows, cols);
+    }
+
+    public void movePieceToBoard(int id) {
+
+    }
+
+    public void movePieceToUserPool(int id, String username) {
+
+    }
+
+    public void capturePiece(int id) {
+
+    }
+
 }
