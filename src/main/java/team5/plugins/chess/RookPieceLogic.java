1b13264afd005bbe292d1609a9a2b72b398dc24f
Jman012 <jman012guy@gmail.com>
2017-03-12 14:55:26 -0700

    Chess Rook

--- src/main/java/team5/plugins/chess/RookPieceLogic.java
+++ src/main/java/team5/plugins/chess/RookPieceLogic.java
@@ -3,6 +3,7 @@ package team5.plugins.chess;
 import team5.game.state.Board;
 import team5.game.state.PieceCoordinate;
 import team5.game.state.PieceLogic;
+import team5.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,39 @@ import java.util.List;
 public class RookPieceLogic extends PieceLogic {
 
     public List<PieceCoordinate> moveableCoordinates(Board b, PieceCoordinate pc) {
-        return new ArrayList<PieceCoordinate>();
+        List<PieceCoordinate> coords = new ArrayList<PieceCoordinate>();
+
+        List<Pair<Integer, Integer>> rawCoords = new ArrayList<Pair<Integer, Integer>>() {{
+            add(new Pair<>(1, 0));
+            add(new Pair<>(-1, 0));
+            add(new Pair<>(0, 1));
+            add(new Pair<>(0, -1));
+        }};
+
+        for (Pair<Integer, Integer> delta : rawCoords) {
+            PieceCoordinate workingPC = new PieceCoordinate(pc.getRow(), pc.getColumn());
+
+            while (true) {
+                workingPC.setRow(workingPC.getRow() + delta.getFirst());
+                workingPC.setColumn(workingPC.getColumn() + delta.getFirst());
+
+                if (b.validCoordinate(workingPC)) {
+                    // If the space to the current direction exists, add it
+                    coords.add(new PieceCoordinate(workingPC.getRow(), workingPC.getColumn()));
+
+                    if (b.getPiece(workingPC) != null) {
+                        // If that space also happens to have a piece, then this is the extent
+                        // to which it can move in this direction
+                        break;
+                    }
+                } else {
+                    // If it's off the board, stop
+                    break;
+                }
+            }
+        }
+
+        return coords;
     }
 
     public String canChangeToPiece() {
