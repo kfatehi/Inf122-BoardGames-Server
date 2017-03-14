6b95b19cce40c90f98c1bff5aeaae98e95e459c0
Jman012 <jman012guy@gmail.com>
2017-03-14 10:53:30 -0700

    Fix bug with client that wasn't showing valid moves for piece with ID=0

--- src/main/java/team5/game/state/Piece.java
+++ src/main/java/team5/game/state/Piece.java
@@ -4,7 +4,7 @@ package team5.game.state;
  * Created by james on 3/8/17.
  */
 public class Piece {
-    private static int NEXT_UNIQUE_ID = 0;
+    private static int NEXT_UNIQUE_ID = 1;
     
     private int id;
     private String username;
